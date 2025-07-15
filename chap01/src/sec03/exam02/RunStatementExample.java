db37e6bc18ce950a5378406ec67ce21c31d8aacc
juan <dmsgo5866@gmail.com>
2025-07-15 12:25:07 +0900

    커밋 GUI 연습
    주석 한 줄 추가
    
    이슈번호: #111

--- chap01/src/sec03/exam02/RunStatementExample.java
+++ chap01/src/sec03/exam02/RunStatementExample.java
@@ -8,6 +8,8 @@ public class RunStatementExample {
 		int result = x + y ; // 변수 result를 선언하고 x와 y를 더한 값을 저장
 		System.out.println(result); // 콘솔에 출력
 		// git 업데이트 추가
+		
+		// commit 메시지 연습
 	}
 
 }
